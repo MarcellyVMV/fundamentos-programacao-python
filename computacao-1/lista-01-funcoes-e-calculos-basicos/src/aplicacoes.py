a4515d79008494f2d2928da09fdd3fb388435d9f
Marcelly Victória Marques Veiga <marcellyvmveiga@gmail.com>
2026-04-08 15:04:51 -0300

    Realização de Aplicações

--- computacao-1/lista-01-funcoes-e-calculos-basicos/src/aplicacoes.py
+++ computacao-1/lista-01-funcoes-e-calculos-basicos/src/aplicacoes.py
@@ -1,6 +1,5 @@
 # 8. Defina uma função que, dado o valor da conta de um restaurante, calcule a gorjeta do garçom, considerando que a gorjeta deve ser 15% do valor da conta.
 
-
 def gorjeta(conta):
     """
     Calcula a gorjeta do garçom a partir do valor da conta.
@@ -13,6 +12,7 @@ def gorjeta(conta):
     """
     gorjeta = conta * 0.15
     return gorjeta
+
 
 # 9. Defina uma nova função que, dado o valor da conta de um restaurante e a porcentagem exigida pela legislação para a gorjeta, calcule o valor dessa gorjeta.
 
@@ -27,7 +27,8 @@ def gorjeta_legislacao(conta, porcentagem):
     Returns:
     gorjeta_legislacao (float): Valor da gorjeta
     """
-    gorjeta_legislacao = conta * (porcentagem / 100) #Digitar a porcentagem como um número inteiro. Exemplo: 10 = 10%.
+    # Digitar a porcentagem como um número inteiro. Exemplo: 10 = 10%.
+    gorjeta_legislacao = conta * (porcentagem / 100)
     return gorjeta_legislacao
 
 
@@ -45,8 +46,25 @@ def saldo_final(capital, taxa, tempo):
     Returns:
     saldo_final (float): Saldo final da conta
     """
-    saldo_final = capital * (1 + (taxa / 100) * tempo) #Digitar a taxa como um número inteiro. Exemplo: 5 = 5%. e o tempo em meses inteiros. Exemplo: 12 = 12 meses.
+    # Digitar a taxa como um número inteiro. Exemplo: 5 = 5%. e o tempo em meses inteiros. Exemplo: 12 = 12 meses.
+    saldo_final = capital * (1 + (taxa / 100) * tempo)
     return saldo_final
+
 
 # 11. Defina uma função que calcule a distância que a correnteza arrasta um barco que atravessa um rio.
 
+def arrasta(vel_correnteza, largura, vel_barco):
+    """
+    Calcula a distância que a correnteza arrasta um barco que atravessa um rio.
+
+    Parameters:
+    vel_correnteza (float): Velocidade da correnteza
+    largura (float): Largura do rio
+    vel_barco (float): Velocidade do barco
+
+    Returns:
+    arrasta (float): Distância que a correnteza arrasta o barco
+    """
+    # Digitar na mesma unidade. Exemplo: Se vel_correnteza for em m/s, a largura deve ser em m e a vel_barco em m/s.
+    arrasta = vel_correnteza * (largura / vel_barco)
+    return arrasta
